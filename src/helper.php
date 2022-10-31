79fa4e429e7a097443760636d6ee75e7f55d5d07
reaway <reaway@foxmail.com>
2022-10-31 22:21:23 +0800

    调整一些注解

--- src/helper.php
+++ src/helper.php
@@ -15,10 +15,11 @@ use Think\Component\Container\Container;
 if (!function_exists('container')) {
     /**
      * 快速获取容器中的实例 支持依赖注入
-     * @param string $name 类名或标识 默认获取当前应用实例
-     * @param array $args 参数
-     * @param bool $newInstance 是否每次创建新的实例
-     * @return object
+     * @template T
+     * @param string|class-string<T> $name        类名或标识 默认获取当前应用实例
+     * @param array                  $args        参数
+     * @param bool                   $newInstance 是否每次创建新的实例
+     * @return T|object|Container
      */
     function container(string $name = '', array $args = [], bool $newInstance = false)
     {
